cb0250de666047234813d0ed9d6df990495aea61
sawepeter <sawepeter6@gmail.com>
2019-09-06 01:43:23 +0300

    initial commit

---
+++ app/src/main/java/com/example/photorepo/ClientDashboard.java
@@ -0,0 +1,14 @@
+package com.example.photorepo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ClientDashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_client_dashboard);
+    }
+}
