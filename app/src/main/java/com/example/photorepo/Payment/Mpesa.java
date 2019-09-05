76680b57d48c0419556aa30676a706c1a7eb0c80
sawepeter <sawepeter6@gmail.com>
2019-09-06 02:18:12 +0300

    initial commit

---
+++ app/src/main/java/com/example/photorepo/Payment/Mpesa.java
@@ -0,0 +1,16 @@
+package com.example.photorepo.Payment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.photorepo.R;
+
+public class Mpesa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mpesa);
+    }
+}
