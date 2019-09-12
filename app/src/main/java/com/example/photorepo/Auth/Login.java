8b180417178d9ddd7ee222a6a75b5bf0791474b6
sawepeter <sawepeter6@gmail.com>
2019-09-12 15:02:07 +0300

    changes

--- app/src/main/java/com/example/photorepo/Auth/Login.java
+++ app/src/main/java/com/example/photorepo/Auth/Login.java
@@ -5,13 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.example.photorepo.ClientDashboard;
+import com.example.photorepo.PhotographerDashboard;
 import com.example.photorepo.R;
 
 public class Login extends AppCompatActivity {
+    Spinner spinner1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,18 +27,46 @@ public class Login extends AppCompatActivity {
         final  EditText Password = findViewById(R.id.lpassword);
         final Button btn_login = findViewById(R.id.btn_login);
 
+        spinner1 = findViewById(R.id.spinner1);
+        spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                int index = spinner1.getSelectedItemPosition();
+                if (spinner1.getSelectedItem().toString().equalsIgnoreCase("Client")){
+                    //set onclick listener
+                    btn_login.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Intent intent = new Intent(Login.this, ClientDashboard.class);
+                            startActivity(intent);
+
+                        }
+                    });
+
+                } else if (spinner1.getSelectedItem().toString().equalsIgnoreCase("Photographer")){
+                    btn_login.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Intent intent = new Intent(Login.this, PhotographerDashboard.class);
+                            startActivity(intent);
+
+                        }
+                    });
+
+
+
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
         //get the edit_text and converted them into strings
         String email = edt_email.getText().toString();
         String pass = Password.getText().toString();
 
-        //set onclick listener
-                btn_login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Login.this, ClientDashboard.class);
-                startActivity(intent);
-
-            }
-        });
     }
 }
